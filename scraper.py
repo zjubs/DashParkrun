2a84d53dd79a1b4fb4e146755808609b69a9bb3e
zjubs <zjubs1@gmail.com>
2024-09-22 19:17:28 +0100

    update to clean up app and fix bug with times of over an hour

--- scraper.py
+++ scraper.py
@@ -4,8 +4,8 @@ import requests
 from io import StringIO
 import pandas as pd
 
-def get_results_table(id):
 
+def get_url(id):
     url = f'https://www.parkrun.org.uk/parkrunner/{id}/5k'
 
     headers={
@@ -15,19 +15,62 @@ def get_results_table(id):
     }
 
     page = requests.get(url,headers=headers, verify=False) #verify= False to stop issue with limit
+
+    return page
+
+def convert_to_seconds(time_str: str):
+
+    """
+    converts strings of time in format 10:33 or 01:01:42 into time in seconds
+    """
+
+    parts = time_str.split(':')
+    if len(parts) == 2:
+        time_str = '00:' + time_str
+
+    return pd.to_timedelta(time_str).total_seconds()
+
+    
+
+
+def extract_results_table(page):
+
     soup = BeautifulSoup(page.text, 'html.parser')
 
     # The third table is the table of 5k results
     results_table = soup.find_all('table')[2]
-    #results_table_df = pd.read_html(str(results_table))
 
     html_string_io = StringIO(str(results_table))
     results_table_df = pd.read_html(html_string_io)[0]
 
     # Step 1: Convert the date and time strings
     results_table_df['Run Date'] = pd.to_datetime(results_table_df['Run Date'], format='%d/%m/%Y')
-    results_table_df['Time'] = pd.to_timedelta('00:' + results_table_df['Time']).dt.total_seconds()
+    
+    results_table_df['Time'] = results_table_df['Time'].apply(convert_to_seconds)
 
-    #print(results_table_df)
-    #results_table_df.info()
     return results_table_df
+
+
+def get_results_table(id):
+
+    page = get_url(id)
+
+    results_table_df = extract_results_table(page)
+
+    return results_table_df
+
+if __name__ == "__main__":
+
+    # the functions get us the html for 5k times for a specified parkrun users
+    user_page = get_url(3430977) #1674
+    
+    # we can view the structure of the page using 
+    soup = BeautifulSoup(user_page.text, 'html.parser')
+    print(soup.prettify())
+
+    # we can check the captions for the tables in the document (expect 3 tables)
+    for table in soup.find_all('table'):
+        print(table.caption)
+
+    # we can extract and print a table
+    print(extract_results_table(user_page))
