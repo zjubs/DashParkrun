57696dbd6849fc28f37ee2fc4ca81762a685d9e3
zjubs <zjubs1@gmail.com>
2024-10-27 15:34:29 +0000

    remove commented out code

--- scraper.py
+++ scraper.py
@@ -1,7 +1,5 @@
 
-#from bs4 import BeautifulSoup
 import requests
-#from io import StringIO
 import pandas as pd
 
 
@@ -18,70 +16,7 @@ def get_url(id):
 
     return page
 
-# def convert_to_seconds(time_str: str):
-
-#     """
-#     converts strings of time in format 10:33 or 01:01:42 into time in seconds
-#     """
-
-#     parts = time_str.split(':')
-#     if len(parts) == 2:
-#         time_str = '00:' + time_str
-
-#     return pd.to_timedelta(time_str).total_seconds()
-
-    
-
-
-# def extract_results_table(page):
-
-#     soup = BeautifulSoup(page.text, 'html.parser')
-
-#     # The third table is the table of 5k results
-#     results_table = soup.find_all('table')[2]
-
-#     html_string_io = StringIO(str(results_table))
-#     results_table_df = pd.read_html(html_string_io)[0]
-
-#     # Step 1: Convert the date and time strings
-#     results_table_df['Run Date'] = pd.to_datetime(results_table_df['Run Date'], format='%d/%m/%Y')
-    
-#     results_table_df['Time'] = results_table_df['Time'].apply(convert_to_seconds)
-
-#     return results_table_df
-
-
-# def get_results_table(id):
-
-#     page = get_url(id)
-
-#     results_table_df = extract_results_table(page)
-
-#     return results_table_df
 
 if __name__ == "__main__":
 
-    # the functions get us the html for 5k times for a specified parkrun users
     user_page = get_url(1674) #1674
-    #with open("ExampleInputJunior.html", "w", encoding="utf-8") as file:
-    #    file.write(user_page.text)
-
-    
-    # # we can view the structure of the page using 
-    # soup = BeautifulSoup(user_page.text, 'html.parser')
-    # print(soup.prettify())
-
-    # # user name
-    # user_string = soup.h2.get_text()
-    # print(user_string)
-
-    # name_string = user_string.split('\xa0')[0]
-    # print(name_string)
-    
-
-    # # we can check the captions for the tables in the document (expect 3 tables)
-    # for table in soup.find_all('table'):
-    #     print(table.caption)
-
-    # # we can extract and print a table
-    # print(get_results_table(user_page))
